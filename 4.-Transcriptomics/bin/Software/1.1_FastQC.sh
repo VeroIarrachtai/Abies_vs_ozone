38ccccadf6c217f9e9c6b6a3579c6486c851cee6
VeroIarrachtai <veronica.rg.pb@gmail.com>
2020-06-13 18:51:09 -0500

    check before and after fastqc

--- 4.-Transcriptomics/bin/Software/1.1_FastQC.sh
+++ 4.-Transcriptomics/bin/Software/1.1_FastQC.sh
@@ -3,13 +3,12 @@
 #SBATCH -w nodo3
 #SBATCH -n 4
 
-# Veronica Reyes 
+# Veronica Reyes
 # Paper:
-# Make fastQC analyses 
+# Make fastQC analyses
 
 
 # Do fastqc
 for f in ../../data/RAW/*.fastq.gz;
-do fastqc --outdir  ../../metadata/fastqc_after_trimm/ $f
+do fastqc --outdir  ../../metadata/fastqc_before_trimm/ $f
 done
-
--- 4.-Transcriptomics/bin/Software/2.1_Trimming.sh
+++ 4.-Transcriptomics/bin/Software/2.1_Trimming.sh
@@ -3,9 +3,9 @@
 #SBATCH -w nodo3
 #SBATCH -n 4
 
-# Veronica Reyes 
+# Veronica Reyes
 # Paper:
-# Make trimming and fastQC analyses 
+# Make trimming and fastQC analyses
 
 
 # Do Trimmer  with Trimmomatic-0.36
@@ -14,9 +14,8 @@ for f in DPVR1_S179 DPVR2_S180 DPVR3_S181 DPVR4_S182 DPVR5_S183 DPVR6_S184 DPVR7
 do java -jar ../../../../Programas/Trimmomatic/Trimmomatic_bin/Trimmomatic-0.36/trimmomatic-0.36.jar PE -threads 4 -phred33 ../../data/RAW/${f}_L007_R1_001.fastq.gz ../../data/RAW/${f}_L007_R2_001.fastq.gz ../../data/TRIMMING/Trimmer_${f}_L007_R1_001_paired.fq.gz ../../data/TRIMMING/Trimmer_${f}_L007_R1_001_unpaired.fq.gz ../../data/TRIMMING/Trimmer_${f}_L007_R2_001_paired.fq.gz ../../data/TRIMMING/Trimmer_${f}_L007_R2_001_unpaired.fq.gz ILLUMINACLIP:../../../../Programas/Trimmomatic/Trimmomatic_bin/Trimmomatic-0.36/adapters/TruSeq3-PE-2.fa:2:30:10 LEADING:28 TRAILING:28 SLIDINGWINDOW:10:28 MINLEN:50 HEADCROP:13
 done
 
-# Do fastqc before trimming
+# Do fastqc after trimming
 
-for f in ../../data/TRIMMING/*.fastq.gz;
-do fastqc --outdir  ../../metadata/fastqc_before_trimm/ $f
+for f in ../../data/TRIMMING/*.fq.gz;
+do fastqc --outdir  ../../metadata/fastqc_after_trimm/ $f
 done
-
