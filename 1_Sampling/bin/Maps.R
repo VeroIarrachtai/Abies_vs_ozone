ce154eb2b241a9733018a27124626e71a59bcab5
VeroIarrachtai <veronica.rg.pb@gmail.com>
2020-08-05 20:57:19 -0500

    add 1_Sampling

---
+++ 1_Sampling/bin/Maps.R
@@ -0,0 +1,44 @@
+#https://www.diegovalle.net/mxmaps/
+# Verónica Reyes, febrero 2020
+# Create CDMX´s map
+
+# Install
+#if (!require("devtools")) {
+#  install.packages("devtools")
+#}
+#devtools::install_github("diegovalle/mxmaps")
+
+library("mxmaps")
+library(ggplot2)
+
+
+library("rnaturalearth")
+library("rnaturalearthdata")
+library("ggspatial")
+
+world <- ne_countries(scale = "medium", returnclass = "sf")
+
+ggplot(data = world) +
+  geom_sf() +
+  annotation_scale(location = "bl", width_hint = 0.25) +
+  annotation_north_arrow(location = "bl", which_north = "true", 
+                         pad_x = unit(0.25, "in"), pad_y = unit(0.25, "in"),
+                         style = north_arrow_fancy_orienteering, height = unit(1.25, "cm")) +
+  coord_sf(xlim = c(-116.15, -75.12), ylim = c(10.65, 33.97))+
+  theme_dark()+
+  annotate("point", x = -99.301, y = 19.285, colour = "#4cacdb", size = 2)+
+  labs(x="", y="")
+
+
+data("df_mxmunicipio")
+df_mxmunicipio$value <- as.numeric(df_mxmunicipio$municipio_name =="La Magdalena Contreras")
+
+
+mxmunicipio_choropleth(df_mxmunicipio, num_colors = 2,
+                       zoom = subset(df_mxmunicipio, state_abbr_official %in% "DF")$region) 
+
+palette()
+
+
+
+
