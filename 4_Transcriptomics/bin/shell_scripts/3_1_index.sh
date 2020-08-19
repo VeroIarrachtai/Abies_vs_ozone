43129a5558e3603515f4aa4347c80d3935487441
VeroIarrachtai <veronica.rg.pb@gmail.com>
2020-08-18 22:35:57 -0500

    add script

--- 4_Transcriptomics/bin/shell_scripts/3_1_index.sh
+++ 4_Transcriptomics/bin/shell_scripts/3_1_index.sh
@@ -4,7 +4,7 @@
 #SBATCH -n 4
 
 # Veronica Reyes 
-# Paper:
+# Paper: Abies vs ozone
 # Make index to alignment
 
 bwa index -p ../../metadata/Index/index_Areligiosa -a is ../../metadata/Reference_Transcriptome/GCAT_AB-RNA-1.0.16.fa 
