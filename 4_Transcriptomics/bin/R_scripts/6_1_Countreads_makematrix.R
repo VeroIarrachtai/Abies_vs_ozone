d8783b343fec6a897949b4665ef63d4e5995436f
VeroIarrachtai <veronica.rg.pb@gmail.com>
2020-08-22 23:32:38 -0500

    change H to T

--- 4_Transcriptomics/bin/R_scripts/6_1_Countreads_makematrix.R
+++ 4_Transcriptomics/bin/R_scripts/6_1_Countreads_makematrix.R
@@ -10,15 +10,15 @@ library(readr)
 
 # Load files
 
-HC01_15<-read.delim("../../data/TXT/GENES_ORDER/DPVR1_S179_sw10L50.genesorder.txt", header= FALSE)
-HC02_15<-read.delim("../../data/TXT/GENES_ORDER/DPVR2_S180_sw10L50.genesorder.txt", header= FALSE)
-HC03_15<-read.delim("../../data/TXT/GENES_ORDER/DPVR3_S181_sw10L50.genesorder.txt", header= FALSE)
-HC04_15<-read.delim("../../data/TXT/GENES_ORDER/DPVR4_S182_sw10L50.genesorder.txt", header= FALSE)
-HC05_15<-read.delim("../../data/TXT/GENES_ORDER/DPVR5_S183_sw10L50.genesorder.txt", header= FALSE)
+TC01_15<-read.delim("../../data/TXT/GENES_ORDER/DPVR1_S179_sw10L50.genesorder.txt", header= FALSE)
+TC02_15<-read.delim("../../data/TXT/GENES_ORDER/DPVR2_S180_sw10L50.genesorder.txt", header= FALSE)
+TC03_15<-read.delim("../../data/TXT/GENES_ORDER/DPVR3_S181_sw10L50.genesorder.txt", header= FALSE)
+TC04_15<-read.delim("../../data/TXT/GENES_ORDER/DPVR4_S182_sw10L50.genesorder.txt", header= FALSE)
+TC05_15<-read.delim("../../data/TXT/GENES_ORDER/DPVR5_S183_sw10L50.genesorder.txt", header= FALSE)
 
-HS01_15<-read.delim("../../data/TXT/GENES_ORDER/DPVR11_S189_sw10L50.genesorder.txt", header= FALSE)
-HS02_15<-read.delim("../../data/TXT/GENES_ORDER/DPVR12_S190_sw10L50.genesorder.txt", header= FALSE)
-HS05_15<-read.delim("../../data/TXT/GENES_ORDER/DPVR13_S191_sw10L50.genesorder.txt", header= FALSE)
+TS01_15<-read.delim("../../data/TXT/GENES_ORDER/DPVR11_S189_sw10L50.genesorder.txt", header= FALSE)
+TS02_15<-read.delim("../../data/TXT/GENES_ORDER/DPVR12_S190_sw10L50.genesorder.txt", header= FALSE)
+TS05_15<-read.delim("../../data/TXT/GENES_ORDER/DPVR13_S191_sw10L50.genesorder.txt", header= FALSE)
 
 DC01_15<-read.delim("../../data/TXT/GENES_ORDER/DPVR6_S184_sw10L50.genesorder.txt", header= FALSE)
 DC02_15<-read.delim("../../data/TXT/GENES_ORDER/DPVR7_S185_sw10L50.genesorder.txt", header= FALSE)
@@ -30,7 +30,7 @@ DS01_15<-read.delim("../../data/TXT/GENES_ORDER/DPVR14_S192_sw10L50.genesorder.t
 DS02_15<-read.delim("../../data/TXT/GENES_ORDER/DPVR15_S193_sw10L50.genesorder.txt", header= FALSE)
 DS04_15<-read.delim("../../data/TXT/GENES_ORDER/DPVR16_S194_sw10L50.genesorder.txt", header= FALSE)
 
-HC01_17<-read.delim("../../data/TXT/GENES_ORDER/DPVR17_S195_sw10L50.genesorder.txt", header= FALSE)
+TC01_17<-read.delim("../../data/TXT/GENES_ORDER/DPVR17_S195_sw10L50.genesorder.txt", header= FALSE)
 DC04_17<-read.delim("../../data/TXT/GENES_ORDER/DPVR18_S196_sw10L50.genesorder.txt", header= FALSE)
 
 head(DC04_17)
@@ -46,17 +46,17 @@ colnames(DS01_15)<- c("number_reads", "name_gen")
 colnames(DS02_15)<- c("number_reads", "name_gen")
 colnames(DS04_15)<- c("number_reads", "name_gen")
 
-colnames(HC01_15)<- c("number_reads", "name_gen")
-colnames(HC02_15)<- c("number_reads", "name_gen")
-colnames(HC03_15)<- c("number_reads", "name_gen")
-colnames(HC04_15)<- c("number_reads", "name_gen")
-colnames(HC05_15)<- c("number_reads", "name_gen")
+colnames(TC01_15)<- c("number_reads", "name_gen")
+colnames(TC02_15)<- c("number_reads", "name_gen")
+colnames(TC03_15)<- c("number_reads", "name_gen")
+colnames(TC04_15)<- c("number_reads", "name_gen")
+colnames(TC05_15)<- c("number_reads", "name_gen")
 
-colnames(HS01_15)<- c("number_reads", "name_gen")
-colnames(HS02_15)<- c("number_reads", "name_gen")
-colnames(HS05_15)<- c("number_reads", "name_gen")
+colnames(TS01_15)<- c("number_reads", "name_gen")
+colnames(TS02_15)<- c("number_reads", "name_gen")
+colnames(TS05_15)<- c("number_reads", "name_gen")
 
-colnames(HC01_17)<- c("number_reads", "name_gen")
+colnames(TC01_17)<- c("number_reads", "name_gen")
 colnames(DC04_17)<- c("number_reads", "name_gen")
 head(DC04_17)
 
@@ -73,17 +73,17 @@ all$DS_1 <- DS01_15$number_reads[match(all$DC01_15.name_gen, DS01_15$name_gen)]
 all$DS_2 <- DS02_15$number_reads[match(all$DC01_15.name_gen, DS02_15$name_gen)]
 all$DS_4 <- DS04_15$number_reads[match(all$DC01_15.name_gen, DS04_15$name_gen)]
 
-all$TC_1 <- HC01_15$number_reads[match(all$DC01_15.name_gen, HC01_15$name_gen)]
-all$TC_2 <- HC02_15$number_reads[match(all$DC01_15.name_gen, HC02_15$name_gen)]
-all$TC_3 <- HC03_15$number_reads[match(all$DC01_15.name_gen, HC03_15$name_gen)]
-all$TC_4 <- HC04_15$number_reads[match(all$DC01_15.name_gen, HC04_15$name_gen)]
-all$TC_5 <- HC05_15$number_reads[match(all$DC01_15.name_gen, HC05_15$name_gen)]
+all$TC_1 <- TC01_15$number_reads[match(all$DC01_15.name_gen, TC01_15$name_gen)]
+all$TC_2 <- TC02_15$number_reads[match(all$DC01_15.name_gen, TC02_15$name_gen)]
+all$TC_3 <- TC03_15$number_reads[match(all$DC01_15.name_gen, TC03_15$name_gen)]
+all$TC_4 <- TC04_15$number_reads[match(all$DC01_15.name_gen, TC04_15$name_gen)]
+all$TC_5 <- TC05_15$number_reads[match(all$DC01_15.name_gen, TC05_15$name_gen)]
 
-all$TS_1 <- HS01_15$number_reads[match(all$DC01_15.name_gen, HS01_15$name_gen)]
-all$TS_2 <- HS02_15$number_reads[match(all$DC01_15.name_gen, HS02_15$name_gen)]
-all$TS_5 <- HS05_15$number_reads[match(all$DC01_15.name_gen, HS05_15$name_gen)]
+all$TS_1 <- TS01_15$number_reads[match(all$DC01_15.name_gen, TS01_15$name_gen)]
+all$TS_2 <- TS02_15$number_reads[match(all$DC01_15.name_gen, TS02_15$name_gen)]
+all$TS_5 <- TS05_15$number_reads[match(all$DC01_15.name_gen, TS05_15$name_gen)]
 
-all$TC17 <- HC01_17$number_reads[match(all$DC01_15.name_gen, HC01_17$name_gen)]
+all$TC17 <- TC01_17$number_reads[match(all$DC01_15.name_gen, TC01_17$name_gen)]
 all$DC47 <- DC04_17$number_reads[match(all$DC01_15.name_gen, DC04_17$name_gen)]
 
 head(all)
