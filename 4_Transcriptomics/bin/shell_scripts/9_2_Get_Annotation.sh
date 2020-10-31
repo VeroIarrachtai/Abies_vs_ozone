e8bad441f43d5259fd289199d9954d3af3ca8af2
VeroIarrachtai <veronica.rg.pb@gmail.com>
2020-10-30 19:09:33 -0600

    add P-fam data

--- 4_Transcriptomics/bin/shell_scripts/9_2_Get_Annotation.sh
+++ 4_Transcriptomics/bin/shell_scripts/9_2_Get_Annotation.sh
@@ -12,14 +12,14 @@
 
 # Extract data BEST ORF
 
-for i in $(cat ../../data/Over_Down/SPECIFIC/L_D2_ER_C_TvsD_IDs_over.txt) ;do grep -A1 "$i" ../../../../../../Descargas/GCAT_AB-RNA-1.0.16.Pfam.txt; done > ../../data/Over_Down/SPECIFIC/P-fam_D2_ER_C_TvsD_over.txt
-for i in $(cat ../../data/Over_Down/SPECIFIC/L_D2_ER_C_TvsD_IDs_down.txt) ;do grep -A1 "$i" ../../../../../../Descargas/GCAT_AB-RNA-1.0.16.Pfam.txt; done > ../../data/Over_Down/SPECIFIC/P-fam_D2_ER_C_TvsD_down.txt
+for i in $(cat ../../data/Over_Down/SPECIFIC/L_D2_ER_C_TvsD_IDs_over.txt) ;do grep -A0 "$i" ../../../../../../Descargas/GCAT_AB-RNA-1.0.16.Pfam.txt; done > ../../data/Over_Down/SPECIFIC/P-fam_D2_ER_C_TvsD_over.txt
+for i in $(cat ../../data/Over_Down/SPECIFIC/L_D2_ER_C_TvsD_IDs_down.txt) ;do grep -A0 "$i" ../../../../../../Descargas/GCAT_AB-RNA-1.0.16.Pfam.txt; done > ../../data/Over_Down/SPECIFIC/P-fam_D2_ER_C_TvsD_down.txt
 
-for i in $(cat ../../data/Over_Down/SPECIFIC/L_D2_ER_S_TvsD_IDs_over.txt) ;do grep -A1 "$i" ../../../../../../Descargas/GCAT_AB-RNA-1.0.16.Pfam.txt; done > ../../data/Over_Down/SPECIFIC/P-fam_D2_ER_S_TvsD_over.txt
-for i in $(cat ../../data/Over_Down/SPECIFIC/L_D2_ER_S_TvsD_IDs_down.txt) ;do grep -A1 "$i" ../../../../../../Descargas/GCAT_AB-RNA-1.0.16.Pfam.txt; done > ../../data/Over_Down/SPECIFIC/P-fam_D2_ER_S_TvsD_down.txt
+for i in $(cat ../../data/Over_Down/SPECIFIC/L_D2_ER_S_TvsD_IDs_over.txt) ;do grep -A0 "$i" ../../../../../../Descargas/GCAT_AB-RNA-1.0.16.Pfam.txt; done > ../../data/Over_Down/SPECIFIC/P-fam_D2_ER_S_TvsD_over.txt
+for i in $(cat ../../data/Over_Down/SPECIFIC/L_D2_ER_S_TvsD_IDs_down.txt) ;do grep -A0 "$i" ../../../../../../Descargas/GCAT_AB-RNA-1.0.16.Pfam.txt; done > ../../data/Over_Down/SPECIFIC/P-fam_D2_ER_S_TvsD_down.txt
 
-for i in $(cat ../../data/Over_Down/SPECIFIC/L_D2_ER_D_170Cvs87SS_IDs_over.txt) ;do grep -A1 "$i" ../../../../../../Descargas/GCAT_AB-RNA-1.0.16.Pfam.txt; done > ../../data/Over_Down/SPECIFIC/P-fam_D2_ER_D_170Cvs87SS_over.txt
-for i in $(cat ../../data/Over_Down/SPECIFIC/L_D2_ER_D_170Cvs87SS_IDs_down.txt) ;do grep -A1 "$i" ../../../../../../Descargas/GCAT_AB-RNA-1.0.16.Pfam.txt; done > ../../data/Over_Down/SPECIFIC/P-fam_D2_ER_D_170Cvs87SS_down.txt
+for i in $(cat ../../data/Over_Down/SPECIFIC/L_D2_ER_D_170Cvs87SS_IDs_over.txt) ;do grep -A0 "$i" ../../../../../../Descargas/GCAT_AB-RNA-1.0.16.Pfam.txt; done > ../../data/Over_Down/SPECIFIC/P-fam_D2_ER_D_170Cvs87SS_over.txt
+for i in $(cat ../../data/Over_Down/SPECIFIC/L_D2_ER_D_170Cvs87SS_IDs_down.txt) ;do grep -A0 "$i" ../../../../../../Descargas/GCAT_AB-RNA-1.0.16.Pfam.txt; done > ../../data/Over_Down/SPECIFIC/P-fam_D2_ER_D_170Cvs87SS_down.txt
 
-for i in $(cat ../../data/Over_Down/SPECIFIC/L_D2_ER_T_170Cvs87SS_IDs_over.txt) ;do grep -A1 "$i" ../../../../../../Descargas/GCAT_AB-RNA-1.0.16.Pfam.txt; done > ../../data/Over_Down/SPECIFIC/P-fam_D2_ER_T_170Cvs87SS_over.txt
-for i in $(cat ../../data/Over_Down/SPECIFIC/L_D2_ER_T_170Cvs87SS_IDs_down.txt) ;do grep -A1 "$i" ../../../../../../Descargas/GCAT_AB-RNA-1.0.16.Pfam.txt; done > ../../data/Over_Down/SPECIFIC/P-fam_D2_ER_T_170Cvs87SS_down.txt
+for i in $(cat ../../data/Over_Down/SPECIFIC/L_D2_ER_T_170Cvs87SS_IDs_over.txt) ;do grep -A0 "$i" ../../../../../../Descargas/GCAT_AB-RNA-1.0.16.Pfam.txt; done > ../../data/Over_Down/SPECIFIC/P-fam_D2_ER_T_170Cvs87SS_over.txt
+for i in $(cat ../../data/Over_Down/SPECIFIC/L_D2_ER_T_170Cvs87SS_IDs_down.txt) ;do grep -A0 "$i" ../../../../../../Descargas/GCAT_AB-RNA-1.0.16.Pfam.txt; done > ../../data/Over_Down/SPECIFIC/P-fam_D2_ER_T_170Cvs87SS_down.txt
