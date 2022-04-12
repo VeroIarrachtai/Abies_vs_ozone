86654fabf6dc84dfe0f9b48d2378c0c9f52bacad
VeroIarrachtai <veronica.rg.pb@gmail.com>
2022-04-11 19:33:40 -0500

    add 4_9_1_Get_sequence.sh

--- 4_Transcriptomics/bin/shell_scripts/9_1_Get_sequence.sh
+++ 4_Transcriptomics/bin/shell_scripts/9_1_Get_sequence.sh
@@ -28,14 +28,13 @@ rm ../../data/Over_Down/SPECIFIC/D2_ER_*_IDs_*.txt
 
 # Extract data BEST ORF
 
-for i in $(cat ../../data/Over_Down/SPECIFIC/L_D2_ER_C_TvsD_IDs_over.txt) ;do grep -A1 "$i" ../../../../../../Descargas/GCAT_AB-RNA-1.0.16.BESTORF.fa; done > ../../data/Over_Down/SPECIFIC/BEST-ORF_D2_ER_C_TvsD_over.txt
-for i in $(cat ../../data/Over_Down/SPECIFIC/L_D2_ER_C_TvsD_IDs_down.txt) ;do grep -A1 "$i" ../../../../../../Descargas/GCAT_AB-RNA-1.0.16.BESTORF.fa; done > ../../data/Over_Down/SPECIFIC/BEST-ORF_D2_ER_C_TvsD_down.txt
+for i in $(cat ../../data/Over_Down/SPECIFIC/L_D2_ER_C_TvsD_IDs_over.txt) ;do grep -A1 "$i" ../../metadata/GCAT_AB-RNA-1.0.16.BESTORF.fa; done > ../../data/Over_Down/SPECIFIC/BEST-ORF_D2_ER_C_TvsD_over.txt
+for i in $(cat ../../data/Over_Down/SPECIFIC/L_D2_ER_C_TvsD_IDs_down.txt) ;do grep -A1 "$i" ../../metadata/GCAT_AB-RNA-1.0.16.BESTORF.fa; done > ../../data/Over_Down/SPECIFIC/BEST-ORF_D2_ER_C_TvsD_down.txt
+for i in $(cat ../../data/Over_Down/SPECIFIC/L_D2_ER_S_TvsD_IDs_over.txt) ;do grep -A1 "$i" ../../metadata/GCAT_AB-RNA-1.0.16.BESTORF.fa; done > ../../data/Over_Down/SPECIFIC/BEST-ORF_D2_ER_S_TvsD_over.txt
+for i in $(cat ../../data/Over_Down/SPECIFIC/L_D2_ER_S_TvsD_IDs_down.txt) ;do grep -A1 "$i" ../../metadata/GCAT_AB-RNA-1.0.16.BESTORF.fa; done > ../../data/Over_Down/SPECIFIC/BEST-ORF_D2_ER_S_TvsD_down.txt
 
-for i in $(cat ../../data/Over_Down/SPECIFIC/L_D2_ER_S_TvsD_IDs_over.txt) ;do grep -A1 "$i" ../../../../../../Descargas/GCAT_AB-RNA-1.0.16.BESTORF.fa; done > ../../data/Over_Down/SPECIFIC/BEST-ORF_D2_ER_S_TvsD_over.txt
-for i in $(cat ../../data/Over_Down/SPECIFIC/L_D2_ER_S_TvsD_IDs_down.txt) ;do grep -A1 "$i" ../../../../../../Descargas/GCAT_AB-RNA-1.0.16.BESTORF.fa; done > ../../data/Over_Down/SPECIFIC/BEST-ORF_D2_ER_S_TvsD_down.txt
+for i in $(cat ../../data/Over_Down/SPECIFIC/L_D2_ER_D_170Cvs87SS_IDs_over.txt) ;do grep -A1 "$i" ../../metadata/GCAT_AB-RNA-1.0.16.BESTORF.fa; done > ../../data/Over_Down/SPECIFIC/BEST-ORF_D2_ER_D_170Cvs87SS_over.txt
+for i in $(cat ../../data/Over_Down/SPECIFIC/L_D2_ER_D_170Cvs87SS_IDs_down.txt) ;do grep -A1 "$i" ../../metadata/GCAT_AB-RNA-1.0.16.BESTORF.fa; done > ../../data/Over_Down/SPECIFIC/BEST-ORF_D2_ER_D_170Cvs87SS_down.txt
 
-for i in $(cat ../../data/Over_Down/SPECIFIC/L_D2_ER_D_170Cvs87SS_IDs_over.txt) ;do grep -A1 "$i" ../../../../../../Descargas/GCAT_AB-RNA-1.0.16.BESTORF.fa; done > ../../data/Over_Down/SPECIFIC/BEST-ORF_D2_ER_D_170Cvs87SS_over.txt
-for i in $(cat ../../data/Over_Down/SPECIFIC/L_D2_ER_D_170Cvs87SS_IDs_down.txt) ;do grep -A1 "$i" ../../../../../../Descargas/GCAT_AB-RNA-1.0.16.BESTORF.fa; done > ../../data/Over_Down/SPECIFIC/BEST-ORF_D2_ER_D_170Cvs87SS_down.txt
-
-for i in $(cat ../../data/Over_Down/SPECIFIC/L_D2_ER_T_170Cvs87SS_IDs_over.txt) ;do grep -A1 "$i" ../../../../../../Descargas/GCAT_AB-RNA-1.0.16.BESTORF.fa; done > ../../data/Over_Down/SPECIFIC/BEST-ORF_D2_ER_T_170Cvs87SS_over.txt
-for i in $(cat ../../data/Over_Down/SPECIFIC/L_D2_ER_T_170Cvs87SS_IDs_down.txt) ;do grep -A1 "$i" ../../../../../../Descargas/GCAT_AB-RNA-1.0.16.BESTORF.fa; done > ../../data/Over_Down/SPECIFIC/BEST-ORF_D2_ER_T_170Cvs87SS_down.txt
+for i in $(cat ../../data/Over_Down/SPECIFIC/L_D2_ER_T_170Cvs87SS_IDs_over.txt) ;do grep -A1 "$i" ../../metadata/GCAT_AB-RNA-1.0.16.BESTORF.fa; done > ../../data/Over_Down/SPECIFIC/BEST-ORF_D2_ER_T_170Cvs87SS_over.txt
+for i in $(cat ../../data/Over_Down/SPECIFIC/L_D2_ER_T_170Cvs87SS_IDs_down.txt) ;do grep -A1 "$i" ../../metadata/GCAT_AB-RNA-1.0.16.BESTORF.fa; done > ../../data/Over_Down/SPECIFIC/BEST-ORF_D2_ER_T_170Cvs87SS_down.txt
